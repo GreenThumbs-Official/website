3315dcbf7aab511f4e9d47546da66db1841f47b7
Auguste <adollinger@edenschool.fr>
2025-05-14 16:29:46 +0200

    feat(back): started plant migration

---
+++ Back/database/migrations/2025_05_14_141845_create_plant_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('plant', function (Blueprint $table) {
+            $table->ulid('id')->primary();
+            $table->string('name');
+            $table->string('description');
+            $table->string('image');
+            $table->ulid('category_id');
+            $table->ulid('type_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('plant');
+    }
+};
