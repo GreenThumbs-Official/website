225fefa26c291c7f064a565cd54d26440d1f9637
AugusteDollinger <adollinger@edenschool.fr>
2025-06-16 12:05:09 +0200

    feat(routes): Added doc to the routes

--- Back/routes/api.php
+++ Back/routes/api.php
@@ -5,15 +5,29 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 
+
+/**
+ * Routes used for authentification
+ */
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+/**
+ * Test route for API tokens
+ */
+Route::middleware('auth:sanctum')->get('/user-profile', function (Request $request) {
     return $request->user();
 });
 
+/**
+ * Default route used to get all plants
+ */
 Route::apiResource('plants', PlantController::class);
 
+
+/**
+ * Fallback route if resource isn't found
+ */
 Route::fallback(function () {
     return response()->json([
         'error' => 'Route not found',
