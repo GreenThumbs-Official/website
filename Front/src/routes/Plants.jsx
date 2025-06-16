16ed946e44a2b9958da54798e69658ecba354348
HorroGlitch <lverot@edenschool.fr>
2025-06-16 15:47:26 +0200

    inisialize api with plants

--- Front/src/routes/Plants.jsx
+++ Front/src/routes/Plants.jsx
@@ -1,13 +1,28 @@
 function Plants(){
+    
+    async function afficherPlants() {
+
+        const reponse = await fetch("http://127.0.0.1:8000/api/plants");
+        const plant = await reponse.json();
+
+        const stockPlants = document.getElementById('classPlants');
+        const onePlant = document.createElement('div');
+        const plantName = document.createElement('p');
+        const plantImg = document.createElement('img');
+
+        plantName.textContent = plant.name;
+        plantImg.src = plant.img;
+
+        stockPlants.appendChild(onePlant);
+        onePlant.appendChild(plantName);
+        onePlant.appendChild(plantImg);
+        
+    }
+    afficherPlants()
 
     return (
-        <section>
-            <div className="bg-black w-96 h-96">a</div>
-            <div className="bg-black ">a</div>
-            <div className="bg-black ">a</div>
-            <div className="bg-black ">a</div>
-            <div className="bg-black ">a</div>
-            <div className="bg-black w-3xs h-[150]">a</div>
+        <section classPlants="grid grid-cols-3 gap-20">
+
         </section>
     )
 }
