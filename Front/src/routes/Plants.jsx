c82404077ae131582da856e6f77a2c7d7116d160
HorroGlitch <lverot@edenschool.fr>
2025-06-16 14:36:16 +0200

    Plants page created

---
+++ Front/src/routes/Plants.jsx
@@ -0,0 +1,11 @@
+export default function Plants() {
+
+    return (
+        <div className="flex flex-col gap-12 items-start justify-start pt-10 pl-16">
+            <h2 className="text-5xl font-bold">Les plantes en vogue !</h2>
+        </div>
+    )
+
+    
+}
+
