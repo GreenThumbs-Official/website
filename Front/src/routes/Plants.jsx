977878dcca216f5987e7f534b2df6154c5c83c68
HorroGlitch <lverot@edenschool.fr>
2025-06-16 15:16:13 +0200

    testing style

--- Front/src/routes/Plants.jsx
+++ Front/src/routes/Plants.jsx
@@ -1,8 +1,23 @@
-export default function Plants() {
+function Plants(){
+
+    return (
+        <section>
+            <div className="bg-black w-96 h-96">a</div>
+            <div className="bg-black ">a</div>
+            <div className="bg-black ">a</div>
+            <div className="bg-black ">a</div>
+            <div className="bg-black ">a</div>
+            <div className="bg-black w-3xs h-[150]">a</div>
+        </section>
+    )
+}
+
+export default function PlantsPage() {
 
     return (
         <div className="flex flex-col gap-12 items-start justify-start pt-10 pl-16">
-            <h2 className="text-5xl font-bold">Les plantes en vogue !</h2>
+            <h2 className="text-5xl font-bold">Les différentes plantes !</h2>
+            <Plants />
         </div>
     )
 
