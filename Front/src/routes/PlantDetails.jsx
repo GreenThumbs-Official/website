cd637e385ceaa888a37a78eae52abaaaa6340498
HorroGlitch <lverot@edenschool.fr>
2025-06-16 16:29:34 +0200

    inisialize plant detail page

---
+++ Front/src/routes/PlantDetails.jsx
@@ -0,0 +1,11 @@
+export default function PlantDetailPage() {
+
+    return (
+        <div className="flex flex-col gap-12 items-start justify-start pt-10 pl-16">
+            <h2 className="text-5xl font-bold">Page de la plante</h2>
+        </div>
+    )
+
+    
+}
+
