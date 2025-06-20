9c9bcb9c8d1cea33747f7c59e505d57984fe378a
HorroGlitch <lverot@edenschool.fr>
2025-06-20 14:09:30 +0200

    PlantsFollowed page inisialized

---
+++ Front/src/routes/PlantsFollowed.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from '@/components/Header';
+import Background from '@/components/Background';
+
+
+export default function PlantsFollowed() {
+    return (
+        <div className="min-h-screen bg-[#6fbc29] text-white overflow-hidden">
+            <Background />
+            <Header />
+        </div>
+    );
+}
