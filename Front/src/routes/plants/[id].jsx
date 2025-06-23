c6ae3200bf319fbb260b539be3bb800ead3ebbb3
HorroGlitch <lverot@edenschool.fr>
2025-06-23 09:02:59 +0200

    changing API and some values from API

--- Front/src/routes/plants/[id].jsx
+++ Front/src/routes/plants/[id].jsx
@@ -1,8 +1,57 @@
+import React, { useState, useEffect } from 'react';
+import Header from '@/components/Nav/Header';
+import Background from '@/components/ui/background';
+import config from '@/config.json';
+
+function details(){
+
+    async function generatePlantDetail() {
+
+        const reponse = await fetch("https://perenual.com/api/v2/species/details/[ID]?key=sk-7dPj68555c3d615a211096");
+        const plants = await reponse.json();
+
+        plants.data.forEach((plant) =>{
+            let plantStock = document.createElement('div');
+            let plantName = document.createElement('h3');
+            let plantImg = document.createElement('img');
+            let plantLink = document.createElement('a');
+            let buttonView = document.createElement('button');
+
+
+            const plantSection = document.querySelector('.classPlantsFollow')
+ 
+            plantName.textContent = plant.common_name
+            plantImg.setAttribute('src', plant.image_url)
+            plantLink.setAttribute('href', plant.id)
+            plantLink.textContent = "Voir les détails de la plante"
+            console.log(plantImg)
+            console.log(plantName);
+
+            plantStock.appendChild(plantName)
+            plantStock.appendChild(plantImg)
+            buttonView.appendChild(plantLink)
+            console.log(plantSection)            
+            plantSection.appendChild(plantStock)
+        })
+    }
+    generatePlantDetail()
+        
+    return (
+        <section className="classPlantsFollow grid grid-cols-3 gap-20">
+
+        </section>
+    )
+
+}
+
+
 export default function PlantDetailPage() {
 
     return (
-        <div className="flex flex-col gap-12 items-start justify-start pt-10 pl-16">
-            <h2 className="text-5xl font-bold">Page de la plante</h2>
+        <div className="min-h-screen bg-[#6fbc29] text-white overflow-hidden">
+            <Background />
+            <Header />
+            <h2 className="text-5xl font-bold pt-28 pl-12">Page de la plante</h2>
         </div>
     )
 }
